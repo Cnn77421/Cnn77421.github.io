b7ca4ce20eb1125b88ffedfb1998f1c4a45ff856
Ametista_21 <xiner521c@aliyun.com>
2025-10-17 02:08:53 +0800

    Site updated: 2025-10-17 02:08:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/16/hello-world/","2025/10/16/test/"];function toRandomPost(){
+var posts=["2025/10/16/hello-world/","2025/10/16/test/","2025/10/09/测试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
